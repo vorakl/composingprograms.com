beeaf6cf02671c361abfabba644fed677b6af185
Oleksii Tsvietnov <vorakl@protonmail.com>
2019-10-01 19:10:53 +0200

    Formatting accordingly to PEP8

--- pure_oop/main.py
+++ pure_oop/main.py
@@ -3,6 +3,7 @@
 import pure_oop as oop
 from pprint import pprint as pp
 
+
 def make_c1():
     level = 'c1'
     total = 0
@@ -15,24 +16,28 @@ def make_c1():
     def instances(self, amount=None):
         # check if self is a class, not an object
         if '__class__' in self:
-            raise KeyError('ERROR: This is a class function. It cannot be run on an Object!')
+            raise KeyError(
+                        'ERROR: a class function can\'t be run on an object!')
         if amount is not None:
             self['total'] += amount
         return self['total']
 
     return oop.create_class(**locals())
 
+
 def make_c2(*, __super__):
     level = 'c2'
     return oop.create_class(**locals())
+
 
 def make_c3(*, __super__):
     level = 'c3'
 
     def __init__(self):
-        # call class method to update class attribute
-        # this class method exists in c1 but it doesn't matter via which class
-        # to invoke it. The isiest way here is starting right from its own class
+        # Calls a class method to update class attributes.
+        # This class method exists in c1 but it doesn't matter via which
+        # classes to invoke it. The easiest way is starting right from its
+        # own class and a method will be found up through the hierarchy.
         self['__class__']['get']('instances')(1)
 
         # ask a super class to find '__init__' up to the base class
@@ -56,6 +61,7 @@ pp(o2())
 print('\no3 object attributes:')
 pp(o3())
 
-print(f"\nc1's class attribute 'total' requested by o3: {o3('total')}")
-print(f"The same attribute requested via c2's class method: {c2('instances')()}")
-
+print("\nc1's class attribute 'total' requested by o3: {}".format(
+                                                                  o3('total')))
+print("The same attribute requested via c2's class method: {}".format(
+                                                            c2('instances')()))
--- pure_oop/pure_oop.py
+++ pure_oop/pure_oop.py
@@ -1,80 +1,95 @@
 #!/usr/bin/env python3
 
+
 def create_class(*, __super__=None, **kwargs):
-    ''' Creates a 'class', possibly based on already created class
-        All class attributes are expected in the kwargs, e.g. **locals()
+    ''' Creates a class, possibly based on an existing class.
+        All initial class attributes are expected to get in the 'kwargs'
+        argument, e.g. '**locals()'.
+
         Atributes are stored in a dictionary.
-        Entry point to a class is the 'get' function (closure)
+        Entry point to a class is the 'get' function (closure).
     '''
+
     def new(self=None, **kwargs):
         ''' Creates a new object.
-            All object attributes are expected in hte kwargs
+            All object attributes are expected to get in the 'kwargs' argument.
 
-            self is not used and defined as a bypass a general situation,
-            when functions are call with an object or class as a 1st argument
+            self is not used and defined here as a bypass for a general
+            situation, when functions are called with an object or a class
+            as a 1st argument.
 
-            Every object has a reference to its class in the '__class__' key
-
-            Objects are stored in dictionaries and addressed only via the 'get'
-            function (closure) to prevent a direct modification of attributes
+            Every object has a reference to its class in the '__class__'
+            attribute. Objects are stored in dictionaries and accessed via
+            the 'get' function (closure) to prevent a direct modification
+            of attributes.
         '''
+
         __object__ = dict(**kwargs)
         __object__['__class__'] = __class__
 
-        # If an '__init__' function is defined in the class, run it on the object
+        # If the '__init__' function is defined in a class, then run it
+        # for the object
         if '__init__' in __class__:
             __class__['__init__'](__object__)
 
-        # This 'get' function belongs to an Object. 
-        # It's used as en entry point to object's attributes
         def get(attr=None):
+            ''' This 'get' function belongs to an object.
+                It's used as en entry point to the object's attributes.
+            '''
+
             if attr is None:
-                # Without arguments it prints the whole object (dictionary)
-                return __object__ 
+                # With no arguments it prints the whole object (dictionary)
+                return __object__
             elif attr in __object__:
                 return __object__[attr]
             elif attr in __class__:
-                # Didn't find in an object, but found in a class
-                # If it's a function, let's create a method by adding the object
+                # Didn't find in an object, but found in a class.
+                # If it's a function, let's create a method by binding
+                # to the object
                 if callable(__class__[attr]):
-                    def method(*args, **kwargs):
+                    def object_method(*args, **kwargs):
                         return __class__[attr](__object__, *args, **kwargs)
-                    return method
+                    return object_method
                 else:
-                    # If it's a variable, simply return it
+                    # If it's a class attribute (variable), simply return it
                     return __class__[attr]
             elif __class__['__super__'] is not None:
-                # If a class of the object has a super class (it was inherited 
+                # If a class of the object has a super class (it was inherited
                 # from another class), then ask its parent class to find
-                # the attribute up to the base class through the whole hierarchy
-                # and provide it with the object
+                # the attribute up to the base class through the hierarchy.
                 return __class__['__super__'](attr, __object__=__object__)
             else:
                 # There is no super class, which means it's a base class and
                 # there is no the attribute anywhere else
-                raise KeyError(f'ERROR: there is no key {attr} in the base class/object')
+                err = 'ERROR: there is no key {} in the base class/object'
+                raise KeyError((err).format(attr))
+
         return get
 
-    # This 'get' function belongs to a Class. 
-    # It's used as en entry point to class' attributes
-    # 'object' appears as an argument if request originaly came from an object
     def get(attr=None, *, __object__=None):
+        ''' This 'get' function belongs to a class.
+            It's used as en entry point to class' attributes.
+            '__object__' isn't None if request comes from an object.
+        '''
+
         if attr is None:
+            # With no arguments it prints the whole class (dictionary)
             return __class__
         elif attr in __class__:
             if callable(__class__[attr]):
                 if __object__ is not None:
                     # If an object was provided, then request has come from
-                    # that object and it will be set as a first argument.
-                    # Make a method by binding a function of a super class
-                    # and an object (running inherited functions on an object)
+                    # it and this object will be set as a first argument.
+                    # Make a method by binding a function of an object's super
+                    # class and the object (it will run a function from
+                    # an inherited class on an object).
                     def object_method(*args, **kwargs):
                         return __class__[attr](__object__, *args, **kwargs)
                     return object_method
                 else:
                     # When object wasn't provided, that means a class was
                     # queried directly and it will be set as a first argument
-                    # This allows to have a pure class methods which work 
+                    # This allows us to have a pure class methods which work
                     # on thier class attributes only
                     def class_method(*args, **kwargs):
                         return __class__[attr](__class__, *args, **kwargs)
@@ -87,11 +102,11 @@ def create_class(*, __super__=None, **kwargs):
             return __super__(attr, __object__=__object__)
         else:
             # the exception in case of an inheritance
-            raise KeyError(f'ERROR: there is no key "{attr}" in any upper classes')
+            err = 'ERROR: there is no key "{}" in any upper classes'
+            raise KeyError(err).format(attr)
 
-    # Add function's formal parameters and all local names as attributes
-    __class__ = locals()
-    del(__class__['kwargs'])
-    __class__ = { **__class__, **kwargs }
+    # Add local names and key-value pairs from 'kwargs' as class' attributes.
+    __class__ = {**locals(), **kwargs}
+    del(__class__['kwargs'])  # remove unnecessary element from the attributes
 
     return get
